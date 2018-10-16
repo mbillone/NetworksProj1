2d9c124db53bbcad885d997b202a16a863c07ac1
Matt Billone <mbilloe@udel.edu>
2018-10-15 23:43:13 -0400

    added checkBalance function, needs to test

--- tcpserver.c
+++ tcpserver.c
@@ -32,6 +32,17 @@ int convertToInt(char num[20]){
 	return dec;
 }
 
+void checkBalance(char depositAmount[3][20]){
+    if (strncmp(depositAmount[1],"checkings",2)==0){
+        printf("Balance of checkings account: ");
+        printf("%d", checkings);
+    }
+    else if (strncmp(depositAmount[1],"savings",2)==0){
+        printf("Balance of savings account: ");
+        printf("%d", savings);
+    }
+}
+
 void depositCheck(char depositAmount[3][20]){
     /*
     This handles changing deposit amounts. It first calculates the int by calling convertToInt() then it adds this
